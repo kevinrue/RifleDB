ca819088611d8034bb3dfb67eeb257641e5b1374
krue <kevin.rue@ucdconnect.ie>
2014-09-16 00:37:10 +0100

    updated settings to match the PostgreSQL database created for the site.

--- RifleDB/RifleDB/settings.py
+++ RifleDB/RifleDB/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'RifleDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'RifleDB',
+        'USER': 'postgres',
+        'PASSWORD': 'pgSQL',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -68,7 +72,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Eire'
 
 USE_I18N = True
 
