1533feaafbba886d38de0a6d74e4a4ab0eecb764
krue <kevin.rue@ucdconnect.ie>
2014-09-16 09:30:20 +0100

    created models Member and AccessCard

--- RifleDB/attendance/models.py
+++ RifleDB/attendance/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    student_id = models.IntegerField(primary_key=True)
+    #l_name = models.CharField(max_length=100) TODO: login the first name of each member
+    #reg_date = models.DateTimeField('Registration date') TODO: log in the registration date of each member
+
+
+class AccessCard(models.Model):
+    # Django cannot handle primary keys made of multiple columns, let it generate a default one
+    member = models.ForeignKey(Member)
+    rfid = models.BigIntegerField(unique=True)
