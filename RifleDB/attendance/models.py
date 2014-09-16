0f5e80ef9dffec241cb9c5693c207dafdb0d3fa2
krue <kevin.rue@ucdconnect.ie>
2014-09-16 09:48:45 +0100

    added string representation function to Member abd AccessCard

--- RifleDB/attendance/models.py
+++ RifleDB/attendance/models.py
@@ -6,8 +6,14 @@ class Member(models.Model):
     #l_name = models.CharField(max_length=100) TODO: login the first name of each member
     #reg_date = models.DateTimeField('Registration date') TODO: log in the registration date of each member
 
+    def __str__(self):
+        return "%i" % self.student_id
+
 
 class AccessCard(models.Model):
     # Django cannot handle primary keys made of multiple columns, let it generate a default one
     member = models.ForeignKey(Member)
     rfid = models.BigIntegerField(unique=True)
+
+    def __str__(self):
+        return "%i" % self.rfid
