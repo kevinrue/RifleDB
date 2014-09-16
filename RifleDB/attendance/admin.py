9d68ab3bb80969fe44266fad719f09cae4f82f26
krue <kevin.rue@ucdconnect.ie>
2014-09-16 21:24:18 +0100

    Two things. 1: Updated student_id and rfid to character fields to avoid implicit trimming of padding zeros. 2: Added first name and last name fields to member.

--- RifleDB/attendance/admin.py
+++ RifleDB/attendance/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from attendance.models import Member, AccessCard
+
+
+# Customised model admin objects
+class MemberAdmin(admin.ModelAdmin):
+    # formfield_overrides = {
+    # models.: {'widget': TextInput(attrs={'size':'8'})},
+    #     models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
+    fieldsets = [
+        ('Registration', {'fields': ['student_id', 'reg_date']}),
+        ('Identity', {'fields': ['f_name', 'l_name']}),
+        ('Contact details', {'fields': []}),
+        # TODO: admin should also see and update address, phone, card, ... in the same form
+    ]
+    list_display = ('student_id', 'reg_date', 'f_name', 'l_name')
+    readonly_fields = ('reg_date',)
+
+# Register models
+admin.site.register(Member, MemberAdmin)
+admin.site.register(AccessCard)
--- RifleDB/attendance/models.py
+++ RifleDB/attendance/models.py
@@ -1,19 +1,21 @@
+import datetime
+
 from django.db import models
 
-
 class Member(models.Model):
-    student_id = models.IntegerField(primary_key=True)
-    #l_name = models.CharField(max_length=100) TODO: login the first name of each member
-    #reg_date = models.DateTimeField('Registration date') TODO: log in the registration date of each member
+    student_id = models.CharField('Student number', primary_key=True, max_length=8)
+    f_name = models.CharField('First name', max_length=50)
+    l_name = models.CharField('Last name', max_length=50)
+    reg_date = models.DateField('Registration date', default=datetime.datetime.today())
 
     def __str__(self):
-        return "%i" % self.student_id
+        return self.student_id
 
 
 class AccessCard(models.Model):
     # Django cannot handle primary keys made of multiple columns, let it generate a default one
     member = models.ForeignKey(Member)
-    rfid = models.BigIntegerField(unique=True)
+    rfid = models.CharField(unique=True, max_length=12)
 
     def __str__(self):
-        return "%i" % self.rfid
+        return self.rfid
