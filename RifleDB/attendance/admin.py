d98fdd9772ac4c92f3e364aacf2022d92b36749c
krue <kevin.rue@ucdconnect.ie>
2014-09-19 00:33:23 +0100

    Major update to the models and administration. Added the LoggingEvent model to store which card was used to check in or out at what date and time. Also, the AccessCard and LoggingEvent admin list pages borrow information from the Member table to display first and last name corresponding to the card RFID.

--- RifleDB/attendance/admin.py
+++ RifleDB/attendance/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from attendance.models import Member, AccessCard
+from attendance.models import Member, AccessCard, LoggingEvent
 
 
 # Customised model admin objects
 class MemberAdmin(admin.ModelAdmin):
     # formfield_overrides = {
     # models.: {'widget': TextInput(attrs={'size':'8'})},
-    #     models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
     # }
     fieldsets = [
         ('Registration', {'fields': ['student_id', 'reg_date']}),
@@ -15,9 +15,59 @@ class MemberAdmin(admin.ModelAdmin):
         ('Contact details', {'fields': []}),
         # TODO: admin should also see and update address, phone, card, ... in the same form
     ]
-    list_display = ('student_id', 'reg_date', 'f_name', 'l_name')
+    list_display = ('student_id', 'f_name', 'l_name', 'reg_date')
     readonly_fields = ('reg_date',)
+
+
+class AcessCardAdmin(admin.ModelAdmin):
+    # formfield_overrides = {
+    # models.: {'widget': TextInput(attrs={'size':'8'})},
+    # models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
+    fieldsets = [
+        ('Registration', {'fields': ['member', 'rfid', 'reg_date']}),
+    ]
+    list_display = ('rfid', 'member', 'get_f_name', 'get_l_name', 'reg_date')
+    readonly_fields = ('reg_date',)
+
+    def get_f_name(self, obj):
+        return obj.member.f_name
+
+    get_f_name.short_description = 'First name'
+
+    def get_l_name(self, obj):
+        return obj.member.l_name
+
+    get_l_name.short_description = 'Last name'
+
+
+class LoggingEventAdmin(admin.ModelAdmin):
+    # formfield_overrides = {
+    # models.: {'widget': TextInput(attrs={'size':'8'})},
+    # models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
+    fieldsets = [
+        ('Check-in', {'fields': ['check_in', 'rfid']}),
+        ('Check-out', {'fields': ['check_out']}),
+    ]
+    list_display = ('rfid', 'get_f_name', 'get_l_name', 'check_in', 'check_out')
+
+    def get_f_name(self, obj):
+        print(obj.rfid)
+        return AccessCard.objects.get(pk=obj.rfid).member.f_name
+
+    get_f_name.short_description = 'First name'
+
+    def get_l_name(self, obj):
+        print(obj.rfid)
+        return AccessCard.objects.get(pk=obj.rfid).member.l_name
+
+    get_f_name.short_description = 'Last name'
+
 
 # Register models
 admin.site.register(Member, MemberAdmin)
-admin.site.register(AccessCard)
+admin.site.register(AccessCard, AcessCardAdmin)
+admin.site.register(LoggingEvent, LoggingEventAdmin)
+
+members__name__startswith = 'Paul'
--- RifleDB/attendance/models.py
+++ RifleDB/attendance/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 
+
 class Member(models.Model):
     student_id = models.CharField('Student number', primary_key=True, max_length=8)
     f_name = models.CharField('First name', max_length=50)
@@ -15,7 +16,18 @@ class Member(models.Model):
 class AccessCard(models.Model):
     # Django cannot handle primary keys made of multiple columns, let it generate a default one
     member = models.ForeignKey(Member)
-    rfid = models.CharField(unique=True, max_length=12)
+    rfid = models.CharField(primary_key=True, max_length=10)
+    reg_date = models.DateField('Registration date', default=datetime.datetime.today())
 
     def __str__(self):
         return self.rfid
+
+
+class LoggingEvent(models.Model):
+    # Django cannot handle primary keys made of multiple columns, let it generate a default one
+    rfid = models.CharField(max_length=10)
+    check_in = models.DateTimeField('Check in', default=datetime.datetime.today())
+    check_out = models.DateTimeField('Check out', default=None, blank=True, null=True)
+
+    def __str__(self):
+        return "%s from %s to %s" % (self.rfid, self.check_in, self.check_out)
