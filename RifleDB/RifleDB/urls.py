946a81e1320f4967e15fdae00fb71f3697b39a89
krue <kevin.rue@ucdconnect.ie>
2014-09-21 14:14:51 +0100

    simple index page to list the members currently checked-in

--- RifleDB/RifleDB/urls.py
+++ RifleDB/RifleDB/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from attendance import views
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^attendance/', views.index, name='index'),
+    url(r'^$', views.index, name='index'), # base website default is the index of attendance (see below)
+    url(r'^admin/', include(admin.site.urls)), # admin part of the website
+    url(r'^attendance/', views.index, name='index'), # app to check-in/out members and list members checked-in
 )
--- RifleDB/attendance/views.py
+++ RifleDB/attendance/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from attendance.models import Member
 
 def index(request):
     members_checked_in = Member.objects.filter(checked_in=True)
-    output = '\n'.join([p.f_name for p in members_checked_in])
-    return HttpResponse(output)
-
+    context = {'members_checked_in': members_checked_in}
+    return render(request, 'attendance/index.html', context)
